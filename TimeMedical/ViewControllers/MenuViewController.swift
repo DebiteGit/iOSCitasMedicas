ce1fb807dd6a3204ce30aab89be3e04af5e210fb
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-19 00:47:34 -0500

    Add LoginView Popup

--- TimeMedical/ViewControllers/MenuViewController.swift
+++ TimeMedical/ViewControllers/MenuViewController.swift
@@ -13,14 +13,61 @@ class MenuViewController: UIViewController {
 	
 	var gradientLayer = CAGradientLayer()
 	
-    override func viewDidLoad() {
+	@IBOutlet var LoginView: UIView!
+	@IBOutlet weak var VisualEffectView: UIVisualEffectView!
+	
+	var effect:UIVisualEffect!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		effect = VisualEffectView.effect
+		VisualEffectView.effect = nil
 		
 		gradientLayer = view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
 		
 		view.layer.insertSublayer(gradientLayer, at: 0)
 		gradientLayer.frame = view.bounds
+		
+		
+		
+		LoginView.layer.cornerRadius = 10
     }
+	
+	func animateIn() {
+		self.view.addSubview(LoginView)
+		LoginView.center = self.view.center
+		
+		LoginView.transform = CGAffineTransform.init(scaleX: 1.12, y: 1.12)
+		LoginView.alpha = 0
+		
+		self.LoginView.frame = view.bounds
+		
+		UIView.animate(withDuration: 0.4) {
+			self.VisualEffectView.effect = self.effect
+			self.LoginView.alpha = 1
+			self.LoginView.transform = CGAffineTransform.identity
+		}
+	}
+	
+	func animateOut() {
+		UIView.animate(withDuration: 0.3, animations: {
+			self.LoginView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+			self.LoginView.alpha = 0
+			
+			self.VisualEffectView.effect = nil
+			}) { (success:Bool) in
+				self.LoginView.removeFromSuperview()
+			}
+	}
+	
+	@IBAction func LoginAction(_ sender: Any) {
+		animateIn()
+	}
+	
+	@IBAction func AccesoLogin(_ sender: Any) {
+		animateOut()
+	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
@@ -28,12 +75,10 @@ class MenuViewController: UIViewController {
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
-		
 	}
 	
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
-		
 		gradientLayer.frame = view.bounds
 	}
 	
